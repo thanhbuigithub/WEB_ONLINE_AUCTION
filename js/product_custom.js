c93faa2d8fd6d789cadccc56a8ca11572f3f7b9a
Thanh Bui <thanhbuint123@gmail.com>
2019-11-24 17:19:54 +0700

    add post_product.html

--- js/product_custom.js
+++ js/product_custom.js
@@ -412,3 +412,87 @@ $(document).ready(function()
 		});
 	}
 });
+
+/*
+
+21. Init Timer Item
+
+*/
+
+function initTimerList()
+	{
+		if($('.deals_timer_box_item').length)
+		{
+			var timers = $('.deals_timer_box_item');
+			timers.each(function()
+			{
+				var timer = $(this);
+
+				var targetTime;
+				var target_date;
+
+				// Add a date to data-target-time of the .deals_timer_box
+				// Format: "Feb 17, 2018"
+				if(timer.data('target-time') !== "")
+				{
+					targetTime = timer.data('');
+				target_date = new Date(targetTime).getTime();
+				}
+				else
+				{
+					var date = new Date();
+					date.setDate(date.getDate() + 2);
+					target_date = date.getTime();
+				}
+
+			// variables for time units
+			var days, hours, minutes, seconds;
+
+			var h = timer.find('.deals_timer_hr');
+			var m = timer.find('.deals_timer_min');
+			var s = timer.find('.deals_timer_sec');
+			var d = timer.find('.deals_timer_day');
+
+			setInterval(function ()
+			{
+					// find the amount of "seconds" between now and target
+					var current_date = new Date().getTime();
+					var seconds_left = (target_date - current_date) / 1000;
+					console.log(seconds_left);
+
+					// do some time calculations
+					days = parseInt(seconds_left / 86400);
+					seconds_left = seconds_left % 86400;
+
+					hours = parseInt(seconds_left / 3600);
+					hours = hours + days * 24;
+					seconds_left = seconds_left % 3600;
+
+
+					minutes = parseInt(seconds_left / 60);
+					seconds = parseInt(seconds_left % 60);
+
+					if(hours.toString().length < 2)
+					{
+						hours = "0" + hours;
+					}
+					if(minutes.toString().length < 2)
+					{
+						minutes = "0" + minutes;
+					}
+					if(seconds.toString().length < 2)
+					{
+						seconds = "0" + seconds;
+					}
+
+					// display results
+					h.text(hours);
+					m.text(minutes);
+					s.text(seconds);
+					d.text(days)
+
+			}, 1000);
+			});
+		}
+	}
+});
