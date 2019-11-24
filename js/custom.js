a4c4668eb9353db7dfca005d37afb95ffab15fd7
huuthe1999 <55992625+huuthe1999@users.noreply.github.com>
2019-11-24 14:52:07 +0700

    add login + sign up in index.html

--- js/custom.js
+++ js/custom.js
@@ -1114,4 +1114,25 @@ $(document).ready(function()
 					});
 				}
 			}
+			exitLogin();
+			exitSignUp();
+		
+			function exitLogin()
+			{
+				var modal = document.getElementById('login');
+						window.onclick = function(event) {
+						if (event.target == modal) {
+							modal.style.display = "none";
+					}
+				}
+			}
+			function exitSignUp()
+			{
+				var modal = document.getElementById('signup');
+					window.onclick = function(event) {
+						if (event.target == modal) {
+							modal.style.display = "none";
+					}
+				}
+			}	
 });
