4d294e54e964e5fb7d7299944fa5a8343ce06a5e
huuthe1999 <55992625+huuthe1999@users.noreply.github.com>
2020-01-06 19:23:45 +0700

    Add product on category

--- routes/shop.js
+++ routes/shop.js
@@ -2,13 +2,16 @@ var express = require('express');
 var moment = require('moment');
 var router = express.Router();
 const Product = require("../models/product.model");
+const User = require("../models/user.model");
 /* GET shop page. */
 router.get('/', async function (req, res, next) {
   var Products = await Product.instance
     .find()
     .limit(10)
-    .sort({ submit_date: -1 })
+    .sort({ submit_date: 1 })
     .exec();
+
+
   res.render('shop', { title: 'Shop Page', Products: Products});
 });
 
